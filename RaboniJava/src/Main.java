71d2da8323eed5325b63e4129192b53ed7149eed
11ArkaN <149787931+11ArkaN@users.noreply.github.com>
2023-11-24 13:08:58 +0100

    V1

--- RaboniJava/src/Main.java
+++ RaboniJava/src/Main.java
@@ -1,261 +1,151 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Zadanie1(5,2,3));
-        Zadanie2(1,-2,1);
-        Zadanie3(10);
-        Zadanie3While(10);
-        Zadanie3DoWhile(10);
+        System.out.println();
+        System.out.println("Zadanie 1");
+        Zadanie1();
+        System.out.println();
+        System.out.println("Zadanie 2");
+        Zadanie2();
+        System.out.println();
+        System.out.println("Zadanie 3");
+        Zadanie3();
+        System.out.println();
+        System.out.println("Zadanie 4");
+        Zadanie4();
+        System.out.println();
+        System.out.println("Zadanie 5");
+        Zadanie5();
+        System.out.println();
+        System.out.println("Zadanie 6");
+        Zadanie6();
+    }
 
-        double[] kroki = {0.5, 0.1, 0.01};
-        for (double krok : kroki) {
-            double wynik = Zadanie4(krok);
-            System.out.println("Pole dla kroku " + krok + " wynosi " + wynik);
+    public static void Zadanie1(){
+        int[] Array = new int[100];
+        // Wypełnienie tablicy losowymi wartościami z przedziału <0,1000>
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = (int) (Math.random() * 1001);
         }
-
-        double[] kroki2 = {0.5, 0.1, 0.01};
-        for (double krok : kroki2) {
-            double wynik = Zadanie5(krok);
-            System.out.println("Pole dla kroku " + krok + " wynosi " + wynik);
+        // Wyświetlenie co drugiej wartości na ekranie
+        for (int i = 0; i < Array.length; i += 2) {
+            System.out.println(Array[i] + ": " + i);
         }
     }
 
-    public static double Zadanie1(double a, double b, double c){
-        double max = a;
-        if (b > max) max = b;
-        if (c > max) max = c;
-        return max;
-    }
+    public static void Zadanie2(){
+        int[] Array = new int[100];
+        // Wypełnienie tablicy losowymi wartościami z przedziału <0,1000>
+       for (int i = 0; i < Array.length; i++) {
+            int temp = (int) ((Math.random() * 1000));
+            if (temp % 2 == 0){
+                Array[i] = temp;
+            }
+        }
 
-    public static void Zadanie2(double a, double b, double c) {
-        double delta = b * b - 4 * a * c;
-        if (delta > 0) {
-            double x1 = (-b - Math.sqrt(delta)) / (2 * a);
-            double x2 = (-b + Math.sqrt(delta)) / (2 * a);
-            System.out.println("Równanie ma dwa różne pierwiastki rzeczywiste: x1 = " + x1 + " i x2 = " + x2);
-        }
-        else if (delta == 0) {
-            double x = -b / (2 * a);
-            System.out.println("Równanie ma jeden podwójny pierwiastek rzeczywisty: x = " + x);
-        }
-        else {
-            System.out.println("Równanie nie ma pierwiastków rzeczywistych");
+        // Wyświetlenie co drugiej wartości na ekranie
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i] + ": " + i);
         }
     }
 
-    public static void Zadanie3(int count) {
-        if (count <= 0) return;
-        String[][] shapes = new String[count][count];
-
-        for (int i = 0; i < count; i++) {
-            shapes[0][i] = "";
-            for (int j = 0; j < count; j++) {
-                shapes[0][i] += "*";
-            }
-
+    public static void Zadanie3(){
+        int[] Array = new int[10];
+        // Wypełnienie tablicy losowymi wartościami z przedziału <0,100>
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = (int) (Math.random() * 101);
         }
 
-        for (int i = 0; i < count; i++) {
-            shapes[1][i] = "";
-            for (int j = 0; j < i + 1; j++) {
-                shapes[1][i] += "*";
-            }
+        // Invert array
+        for (int i = 0; i < Array.length / 2; i++) {
+            int temp = Array[i];
+            Array[i] = Array[Array.length - i - 1];
+            Array[Array.length - i - 1] = temp;
         }
 
-        for (int i = 0; i < count; i++) {
-            String spaces = String.format("%" + count + "s", "");
-            shapes[2][i] = spaces;
-            for (int j = 0; j < count - i; j++) {
-                shapes[2][i] += "*";
-            }
-        }
-
-        for (int i = 0; i < count; i++) {
-            shapes[3][i] = "";
-            for (int j = 0; j < count - i; j++) {
-                shapes[3][i] += " ";
-            }
-            for (int j = 0; j < i + 1; j++) {
-                shapes[3][i] += "*";
-            }
-            for (int j = 0; j < i; j++) {
-                shapes[3][i] += "*";
-            }
-        }
-
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < count; j++) {
-                if (shapes[j][i] == null) break;
-                System.out.print(shapes[j][i] + "  ");
-            }
-            System.out.print("\n");
-        }
-
-        System.out.print("\n");
-    }
-    public static void Zadanie3While(int count) {
-        if (count <= 0) return;
-        String[][] shapes = new String[count][count];
-
-        int i = 0;
-        while (i < count) {
-            shapes[0][i] = "";
-            int j = 0;
-            while (j < count) {
-                shapes[0][i] += "*";
-                j++;
-            }
-            i++;
-        }
-
-        i = 0;
-        while (i < count) {
-            shapes[1][i] = "";
-            int j = 0;
-            while (j < i + 1) {
-                shapes[1][i] += "*";
-                j++;
-            }
-            i++;
-        }
-
-        i = 0;
-        while (i < count) {
-            String spaces = String.format("%" + count + "s", "");
-            shapes[2][i] = spaces;
-            int j = 0;
-            while (j < count - i) {
-                shapes[2][i] += "*";
-                j++;
-            }
-            i++;
-        }
-
-        i = 0;
-        while (i < count) {
-            shapes[3][i] = "";
-            int j = 0;
-            while (j < count - i) {
-                shapes[3][i] += " ";
-                j++;
-            }
-            j = 0;
-            while (j < i + 1) {
-                shapes[3][i] += "*";
-                j++;
-            }
-            j = 0;
-            while (j < i) {
-                shapes[3][i] += "*";
-                j++;
-            }
-            i++;
-        }
-
-        i = 0;
-        while (i < count) {
-            int j = 0;
-            while (j < count) {
-                if (shapes[j][i] == null) break;
-                System.out.print(shapes[j][i] + "  ");
-                j++;
-            }
-            System.out.print("\n");
-            i++;
+        // Print array
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i] + ": Index:" + i);
         }
     }
 
-    public static void Zadanie3DoWhile(int Count) {
-        if (Count <= 0) return;
-        String[][] shapes = new String[Count][Count];
-        int i = 0;
-        do {
-            shapes[0][i] = "";
-            int j = 0;
-            do {
-                shapes[0][i] += "*";
-                j++;
-            } while (j < Count);
-            i++;
-        } while (i < Count);
-
-        i = 0;
-        do {
-            shapes[1][i] = "";
-            int j = 0;
-            do {
-                shapes[1][i] += "*";
-                j++;
-            } while (j < i + 1);
-            i++;
-        } while (i < Count);
-
-        i = 0;
-        do {
-            String spaces = String.format("%" + Count + "s", "");
-            shapes[2][i] = spaces;
-            int j = 0;
-            do {
-                shapes[2][i] += "*";
-                j++;
-            } while (j < Count - i);
-            i++;
-        } while (i < Count);
-
-        i = 0;
-        do {
-            shapes[3][i] = "";
-            int j = 0;
-            do {
-                shapes[3][i] += " ";
-                j++;
-            } while (j < Count - i);
-            j = 0;
-            do {
-                shapes[3][i] += "*";
-                j++;
-            } while (j < i + 1);
-            j = 0;
-            do {
-                shapes[3][i] += "*";
-                j++;
-            } while (j < i);
-            i++;
-        } while (i < Count);
-
-        i = 0;
-        do {
-            int j = 0;
-            do {
-                if (shapes[j][i] == null) break;
-                System.out.print(shapes[j][i] + "  ");
-                j++;
-            } while (j < 4);
-            System.out.print("\n");
-            i++;
-        } while (i < Count);
-    }
-    public static double Zadanie4(double h) {
-        double pole = 0;
-        double x = -2 + h/2;
-        while (x < 2) {
-            double y = x * x;
-            pole += y * h;
-            x += h;
+    public static void Zadanie4(){
+        int[] Array = new int[100];
+        // Wypełnienie tablicy losowymi wartościami z przedziału <0,100>
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = (int) (Math.random() * 101);
         }
-        return pole;
+
+        // Print array without new lines
+        for (int j : Array) System.out.print(j + " ");
+        System.out.println();
+
+        // Print max array value
+        int max = Array[0];
+        for (int j : Array) if (j > max) max = j;
+        System.out.println("Max: " + max);
+
+        // Print min value
+        int min = Array[0];
+        for (int j : Array) if (j < min) min = j;
+        System.out.println("Min: " + min);
+
+        // Print average value
+        int sum = 0;
+        for (int j : Array) sum += j;
+        int average = sum / Array.length;
+        System.out.println("Average: " + average);
+
+        // Print sum
+        int sum2 = 0;
+        for (int j : Array) sum2 += j;
+        System.out.println("Sum: " + sum2);
     }
 
-    public static double Zadanie5(double h) {
-        double pole = 0;
-        double x = h/2;
-        while (x < 2) {
-            double y1 = x * x;
-            double y2 = 2 * x;
-            pole += (y1 + y2) * h;
-            x += h;
+    public static void Zadanie5(){
+        int[][] Array = new int[100][100];
+        for (int i = 0; i < Array.length; i++) {
+            for (int j = 0; j < Array.length; j++){
+                Array[i][j] = (int) (Math.random() * 101);
+            }
         }
-        return pole;
+
+        // Print diagonal values
+        for (int i = 0; i < Array.length; i++) System.out.print(Array[i][i] + ",");
+        System.out.println();
+        // Print diagonal values from the other side
+        for (int i = 0; i < Array.length; i++) System.out.print(Array[i][Array.length - i - 1] + ",");
     }
 
+    public static void Zadanie6(){
+        int [][] Array = new int[100][100];
+        for (int i = 0; i < Array.length; i++) {
+            for (int j = 0; j < Array.length; j++){
+                Array[i][j] = (int) (Math.random() * 101);
+            }
+        }
+
+        int max = Array[0][0];
+        for (int[] ints : Array) {
+            for (int j = 0; j < Array.length; j++) {
+                if (ints[j] > max) max = ints[j];
+            }
+        }
+        System.out.println("Max: " + max);
+
+        int min = Array[0][0];
+        for (int[] ints : Array) {
+            for (int j = 0; j < Array.length; j++) {
+                if (ints[j] < min) min = ints[j];
+            }
+        }
+        System.out.println("Min: " + min);
+
+        int sum = 0;
+        for (int[] ints : Array) for (int j = 0; j < Array.length; j++) sum += ints[j];
+        System.out.println("Sum: " + sum);
+
+        int average = sum / (Array.length * Array.length);
+        System.out.println("Average: " + average);
+    }
 }
